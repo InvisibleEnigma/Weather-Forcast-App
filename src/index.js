ecf6d5a397350852f54cbbb535f5fbeeb38c9468
InvisibleEnigma <140152285+InvisibleEnigma@users.noreply.github.com>
2024-06-16 19:48:17 +1000

    added search function using api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+ 
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCurrentCity(city) {
+  let apiKey = "4408ef6c23fad2o2b567b019337f885t";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCurrentCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
- searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", searchCity);
+ 
+searchCurrentCity("Brisbane");
